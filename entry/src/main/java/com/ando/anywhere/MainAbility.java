96530fd907935858523e02781e287271388867a1
javakam <jooybao@foxmail.com>
2021-04-14 16:36:36 +0800

    practice

--- entry/src/main/java/com/ando/anywhere/MainAbility.java
+++ entry/src/main/java/com/ando/anywhere/MainAbility.java
@@ -3,12 +3,24 @@ package com.ando.anywhere;
 import com.ando.anywhere.slice.MainAbilitySlice;
 import ohos.aafwk.ability.Ability;
 import ohos.aafwk.content.Intent;
+import ohos.bundle.IBundleManager;
+import ohos.security.SystemPermission;
 
 public class MainAbility extends Ability {
     @Override
     public void onStart(Intent intent) {
         super.onStart(intent);
         super.setMainRoute(MainAbilitySlice.class.getName());
+
+        reqPermission();
+    }
+
+    private void reqPermission() {
+        if (verifySelfPermission(SystemPermission.DISTRIBUTED_DATASYNC) == IBundleManager.PERMISSION_GRANTED) {
+            this.continueAbility();
+        } else {
+            requestPermissionsFromUser(new String[]{SystemPermission.DISTRIBUTED_DATASYNC}, 1);
+        }
     }
 
 }
--- entry/src/main/java/com/ando/anywhere/slice/MainAbilitySlice.java
+++ entry/src/main/java/com/ando/anywhere/slice/MainAbilitySlice.java
@@ -3,12 +3,28 @@ package com.ando.anywhere.slice;
 import com.ando.anywhere.ResourceTable;
 import ohos.aafwk.ability.AbilitySlice;
 import ohos.aafwk.content.Intent;
+import ohos.agp.components.Button;
+import ohos.agp.components.Component;
+import ohos.agp.window.dialog.DisplayDialog;
+import ohos.agp.window.dialog.ToastDialog;
 
 public class MainAbilitySlice extends AbilitySlice {
     @Override
     public void onStart(Intent intent) {
         super.onStart(intent);
         super.setUIContent(ResourceTable.Layout_ability_main);
+
+        final Button btnNext = (Button) findComponentById(ResourceTable.Id_btn_next);
+        btnNext.setClickedListener(new Component.ClickedListener() {
+            @Override
+            public void onClick(Component component) {
+                new ToastDialog(MainAbilitySlice.this)
+                        .setText("下一步")
+                        .setDuration(1000)
+                        .show();
+            }
+        });
+
     }
 
     @Override
